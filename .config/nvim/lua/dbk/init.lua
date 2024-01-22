7abc98e82a997953e30713d94920db807d77b7e4
Deebakkarthi C R <dbk@deebakkarthi.com>
2024-01-22 21:42:31 +0530

    feat: Changed to lazy.nvim

--- .config/nvim/lua/dbk/init.lua
+++ .config/nvim/lua/dbk/init.lua
@@ -1,2 +1,23 @@
 require("dbk.sets")
 require("dbk.remaps")
+-- Put this at the last as leader needs to be set
+require("dbk.lazy")
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+        local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        --vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+        vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+        vim.keymap.set("n", "f", vim.lsp.buf.format, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    end
+})
--- .config/nvim/lua/dbk/remaps.lua
+++ .config/nvim/lua/dbk/remaps.lua
@@ -14,5 +14,3 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
-
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
